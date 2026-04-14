ad234f0a92675eb75f539580c5752c668e15ea20
gideondavis <s1012070@student.knoxschools.org>
2026-04-14 19:52:44 +0000

    more stuff

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db()
+    # Add your new table between lines 15 & 16.
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
